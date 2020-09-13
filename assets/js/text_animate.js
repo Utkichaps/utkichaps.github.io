9a2c771ce7ea1d59aa5e889f61c1d21529201b55
Utkichaps <utkarshchhapekar2@gmail.com>
2020-09-13 13:28:24 +0530

    Changed a few files

---
+++ assets/js/text_animate.js
@@ -0,0 +1,11 @@
+var options = {
+    /*First option getting skipped
+     Have put is clean as default value
+    */
+    strings: ['code^1000','code^1000','build software^1000','love data science^1000','design AI software^1000','play the guitar^1000','develop android applications^1000','act (short films)^1000'],
+    typeSpeed: 40,        
+    loop: true,
+    loopCount: Infinity        
+};
+
+var typed = new Typed('#dynamic', options);
